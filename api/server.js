7bbacf478c8371a0bb211a08c590dc129331c15a
Soraia <soraia@Soraias-iMac.local>
2020-01-15 16:59:49 -0500

    completed mvp for project

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const postsRouter = require('../posts/posts-router.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h2>Lambda Hubs API</h>
+    <p>Welcome to the Lambda Hubs API</p>
+  `);
+});
+
+
+server.use('/api/posts', postsRouter);
+
+
+module.exports = server;
